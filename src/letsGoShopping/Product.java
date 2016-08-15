65492922bf83dcfa3043476d6dd59886eb84eed6
apennybacker@gmail.com <apennybacker@gmail.com>
2016-08-15 14:57:24 -0400

    created new product contstructor

--- src/letsGoShopping/Product.java
+++ src/letsGoShopping/Product.java
@@ -2,38 +2,61 @@
 
 package letsGoShopping;
 
+import java.util.ArrayList;
+
 public class Product {
-	
 
-private String item;
-private String category;
-private int storeQuantity;
-private double price;
+	private String item;
+	private String category;
+	private String itemAlpha;
+	private int storeQuantity;
+	private double price;
 
-	
-public String getItem() {
-	return item;
-}
-public void setItem(String item) {
-	this.item = item;
-}
-public String getCategory() {
-	return category;
-}
-public void setCategory(String category) {
-	this.category = category;
-}
-public int getStoreQuantity() {
-	return storeQuantity;
-}
-public void setStoreQuantity(int storeQuantity) {
-	this.storeQuantity = storeQuantity;
-}
-public double getPrice() {
-	return price;
-}
-public void setPrice(double price) {
-	this.price = price;
-}
+	public Product(String itemAlpha, String item, double price) {
+		setItemAlpha(itemAlpha);
+		setItem(item);
+		setPrice(price);
+
+	}
+
+	public String getItemAlpha() {
+		return itemAlpha;
+	}
+
+	public void setItemAlpha(String itemAlpha) {
+		this.itemAlpha = itemAlpha;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getStoreQuantity() {
+		return storeQuantity;
+	}
+
+	public void setStoreQuantity(int storeQuantity) {
+		this.storeQuantity = storeQuantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }
